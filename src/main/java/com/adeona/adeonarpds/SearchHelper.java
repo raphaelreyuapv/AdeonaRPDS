96869d3d4f293a0f0bfb829a8a911809fa20b0c5
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2022-04-04 10:06:54 +0200

    Added method getUser

--- src/main/java/com/adeona/adeonarpds/SearchHelper.java
+++ src/main/java/com/adeona/adeonarpds/SearchHelper.java
@@ -1,7 +1,6 @@
 package com.adeona.adeonarpds;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
 public class SearchHelper {
 
     public static String url = "jdbc:sqlite:users.sqlite";
@@ -14,5 +13,21 @@ public class SearchHelper {
             System.out.println(e.getMessage());
         }
         System.out.println("yay");
+    }
+
+    public static void getUser(String name){
+        Connection conn = null;
+        Statement statement = null;
+        String query = String.format("SELECT * FROM users WHERE name = '%s'", name);
+        try {
+            conn = DriverManager.getConnection(url);
+            statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString("imageURL"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
