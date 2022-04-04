3289d4789d23f61eed48bcd9de55fb9cdf52f011
sebastien <se.rocca@laposte.net>
2022-04-04 10:51:22 +0200

    Adding the fxml view of stay details and beginning of the controller code

---
+++ src/main/java/com/adeona/adeonarpds/TripDetailsController.java
@@ -0,0 +1,73 @@
+package com.adeona.adeonarpds;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import net.synedra.validatorfx.Check;
+
+public class TripDetailsController {
+
+    @FXML
+    private Label tripTitle;
+
+    @FXML
+    private Label host;
+
+    @FXML
+    private Label place;
+
+    @FXML
+    private Label score;
+
+    @FXML
+    private Label description;
+
+    @FXML
+    private ImageView img1;
+
+    @FXML
+    private ImageView img2;
+
+    @FXML
+    private ImageView img3;
+
+    @FXML
+    private Label chamberNumber;
+
+    @FXML
+    private Label lodgingType;
+
+    @FXML
+    private Label capacity;
+
+    @FXML
+    private Label pension;
+
+    @FXML
+    private CheckBox kitchen;
+
+    @FXML
+    private CheckBox internet;
+
+    @FXML
+    private CheckBox tv;
+
+    @FXML
+    private CheckBox washer;
+
+    @FXML
+    private Button contact;
+
+    private static void test()
+    {
+        System.out.println("fonctionne");
+
+    }
+
+
+
+
+
+}
