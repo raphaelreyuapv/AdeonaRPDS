61669498619d84f2d77b54640d6eb5e6ce863ce3
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2022-04-20 17:20:58 +0200

    Ajout binding composition.fxml

--- src/main/java/com/adeona/adeonarpds/CompositionController.java
+++ src/main/java/com/adeona/adeonarpds/CompositionController.java
@@ -1,4 +1,40 @@
 package com.adeona.adeonarpds;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class CompositionController {
+
+    @FXML
+    private TextField titre;
+    @FXML
+    private TextField lieu;
+    @FXML
+    private TextArea desc;
+    @FXML
+    private TextField url_image;
+    @FXML
+    private TextField pension;
+    @FXML
+    private TextField nombre_chambre;
+    @FXML
+    private ChoiceBox type;
+    @FXML
+    private ChoiceBox capacity;
+    @FXML
+    private CheckBox cuisine;
+    @FXML
+    private CheckBox internet;
+    @FXML
+    private CheckBox television;
+    @FXML
+    private CheckBox lave_linge;
+    @FXML
+    private DatePicker date_debut;
+    @FXML
+    private DatePicker date_fin;
+
+    public void doReservation(){
+
+    }
 }
