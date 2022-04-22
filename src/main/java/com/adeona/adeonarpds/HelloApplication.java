a8c4ba1f877b070d5f8b4d3dc888c5d3618be7dd
Noevain <noevain@gmail.com>
2022-04-22 17:30:07 +0200

    Ajout navigation basic

--- src/main/java/com/adeona/adeonarpds/HelloApplication.java
+++ src/main/java/com/adeona/adeonarpds/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("profilPage-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1500, 1500);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
