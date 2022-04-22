c45dae047f111c0d41a97048a1a4e264b3df134d
Damien Dallon <damien.dallon@alumni.univ-avignon.fr>
2022-04-23 01:53:28 +0200

    Add link to database and threading the display

--- src/main/java/com/adeona/adeonarpds/SearchDisplay.java
+++ src/main/java/com/adeona/adeonarpds/SearchDisplay.java
@@ -1,5 +1,6 @@
 package com.adeona.adeonarpds;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +8,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.concurrent.Task;
 
 import java.io.IOException;
 import java.net.URL;
@@ -22,17 +25,36 @@ public class SearchDisplay implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        Node[] nodes = new Node[20];
+        List<Sejour> allSejours = SearchHelper.getAllSejours();
 
-        for (int i=0; i<nodes.length; i++) {
-            try {
-                nodes[i] = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("search_item.fxml")));
+        Node[] nodes = new Node[allSejours.size()];
 
+        Thread taskThread = new Thread(new Runnable() {
+            @Override public void run() {
+                for (int i=0; i< nodes.length; i++) {
+                    try {
+                        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("search_item.fxml"));
+                        String title = allSejours.get(i).getTitre();
+                        String loc = allSejours.get(i).getLieu();
+                        String host = SearchHelper.getUser(allSejours.get(i).getId_host()).getName();
+                        String img = allSejours.get(i).getURL_image();
+                        fxmlLoader.setControllerFactory(controllerClass -> new SearchItem(title, loc, host, img));
+                        nodes[i] = fxmlLoader.load();
+                        final int index = i;
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                itemHolder.getChildren().add(nodes[index]);
+                            }
+                        });
 
-                itemHolder.getChildren().add(nodes[i]);
-            } catch (IOException e) {
-                e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
+        });
+        taskThread.start();
+
     }
 }
--- src/main/java/com/adeona/adeonarpds/SearchItem.java
+++ src/main/java/com/adeona/adeonarpds/SearchItem.java
@@ -15,25 +15,41 @@ public class SearchItem implements Initializable {
 
     Class<?> clazz = this.getClass();
 
-    InputStream input = clazz.getResourceAsStream("img.png");
-    private Image image = new Image(input);
+    InputStream imageItem;
+
+
+    String titleItem;
+    String locationItem;
+    String hostItem;
 
     @FXML
-    private Label title = new Label("hello");
+    private Label title = null;
 
     @FXML
-    private Label location = new Label("loieu");
+    private Label location = null;
 
     @FXML
-    private Label host = new Label("hote");
-
-
+    private Label host = null;
 
     @FXML
-    private ImageView urlProfile = new ImageView();
+    private Image image = null;
+
+    @FXML
+    private ImageView urlProfile = null;
+
+    public SearchItem(String title, String location, String host, String img) {
+        this.titleItem = title;
+        this.locationItem = location;
+        this.hostItem = host;
+        this.imageItem = clazz.getResourceAsStream(img);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        this.title.setText(this.titleItem);
+        this.location.setText(this.locationItem);
+        this.host.setText(this.hostItem);
+        this.image = new Image(this.imageItem);
+        this.urlProfile.setImage(this.image);
     }
 }
