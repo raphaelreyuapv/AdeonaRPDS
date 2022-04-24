b271ddfa87de874fbd043ae038e9ef864dfa16bc
Damien Dallon <damien.dallon@alumni.univ-avignon.fr>
2022-04-24 23:26:40 +0200

    Link the search function to the search interface and use a service to search and display

--- src/main/java/com/adeona/adeonarpds/SearchDisplay.java
+++ src/main/java/com/adeona/adeonarpds/SearchDisplay.java
@@ -1,13 +1,18 @@
 package com.adeona.adeonarpds;
 
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.concurrent.Task;
@@ -22,40 +27,59 @@ public class SearchDisplay implements Initializable{
     @FXML
     private VBox itemHolder = null;
 
+    @FXML
+    private TextField searchBar = null;
+
+    private List<Sejour> sejoursToDisplay = null;
+
+    Service<Void> service = new Service<Void>()
+    {
+        @Override
+        protected Task<Void> createTask()
+        {
+            Node[] nodes = new Node[sejoursToDisplay.size()];
+
+            return new Task<Void>() {
+                protected Void call() throws Exception {
+                    Thread.sleep(400);
+                    for (int i=0; i< nodes.length; i++) {
+                        try {
+                            final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("search_item.fxml"));
+                            int id = sejoursToDisplay.get(i).getId();
+                            String title = sejoursToDisplay.get(i).getTitre();
+                            String loc = sejoursToDisplay.get(i).getLieu();
+                            String host = SearchHelper.getUser(sejoursToDisplay.get(i).getId_host()).getName();
+                            String img = sejoursToDisplay.get(i).getURL_image();
+                            fxmlLoader.setControllerFactory(controllerClass -> new SearchItem(id, title, loc, host, img));
+                            nodes[i] = fxmlLoader.load();
+                            final int index = i;
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    itemHolder.getChildren().add(nodes[index]);
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    return null;
+                }
+            };
+        }
+    };
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        this.sejoursToDisplay = SearchHelper.getAllSejours();
+        service.start();
+    }
 
-        List<Sejour> allSejours = SearchHelper.getAllSejours();
-
-        Node[] nodes = new Node[allSejours.size()];
-
-        Thread taskThread = new Thread(new Runnable() {
-            @Override public void run() {
-                for (int i=0; i< nodes.length; i++) {
-                    try {
-                        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("search_item.fxml"));
-                        int id = allSejours.get(i).getId();
-                        String title = allSejours.get(i).getTitre();
-                        String loc = allSejours.get(i).getLieu();
-                        String host = SearchHelper.getUser(allSejours.get(i).getId_host()).getName();
-                        String img = allSejours.get(i).getURL_image();
-                        fxmlLoader.setControllerFactory(controllerClass -> new SearchItem(id, title, loc, host, img));
-                        nodes[i] = fxmlLoader.load();
-                        final int index = i;
-                        Platform.runLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                itemHolder.getChildren().add(nodes[index]);
-                            }
-                        });
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        taskThread.start();
-
+    public void search(KeyEvent event) {
+        if(searchBar.getText().length() >= 2) {
+            sejoursToDisplay = SearchHelper.getSejours(searchBar.getText());
+            itemHolder.getChildren().clear();
+            service.restart();
+        }
     }
 }
