dd8392eca5fe6b0dbcb287cb8557f64a049cac55
sebastien <se.rocca@laposte.net>
2022-04-25 23:11:26 +0200

    - deleting code comment

--- src/main/java/com/adeona/adeonarpds/HelloApplication.java
+++ src/main/java/com/adeona/adeonarpds/HelloApplication.java
@@ -21,11 +21,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("rootLayout.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 1000);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();*/
+
         this.mainStage = stage;
         this.mainStage.setResizable(false);
         initRootLayout();
